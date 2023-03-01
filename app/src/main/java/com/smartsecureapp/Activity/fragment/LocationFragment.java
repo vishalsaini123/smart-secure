787e5cb3cc11faf522b2340bb7e18f09c81c6ba7
Puneet <puneet.s@applify.co>
2023-03-01 15:01:20 +0530

    Map implemented.

--- app/src/main/java/com/smartsecureapp/Activity/fragment/LocationFragment.java
+++ app/src/main/java/com/smartsecureapp/Activity/fragment/LocationFragment.java
@@ -1,8 +1,14 @@
 package com.smartsecureapp.Activity.fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Paint;
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,20 +16,86 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.smartsecureapp.R;
 
 
 public class LocationFragment extends Fragment {
-    TextView txt_privacy_policy,txt_term_condition;
+    TextView txt_privacy_policy, txt_term_condition;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int REQUEST_READ_CONTACTS_PERMISSION = 0;
+    Double lat,lng;
+
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestLocationPermission();
+                return;
+            }
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location!=null) {
+                        lat = location.getLatitude();
+                        lng = location.getLongitude();
+                    }else {
+                        lat = 32.3027286;
+                        lng = 75.8818221;
+                    }
+                    LatLng sydney = new LatLng(lat,lng);
+                    googleMap.addMarker(new MarkerOptions().position(sydney).title(""));
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                    googleMap.animateCamera(CameraUpdateFactory.zoomIn());
+                    googleMap.animateCamera(CameraUpdateFactory.zoomTo( 17.0f ));
+                }
+            });
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_location, container, false);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        requestLocationPermission();
         txt_privacy_policy = view.findViewById(R.id.txt_privacy_policy);
         txt_term_condition = view.findViewById(R.id.txt_term_condition);
 
         txt_privacy_policy.setPaintFlags(txt_privacy_policy.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         txt_term_condition.setPaintFlags(txt_term_condition.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+    }
+
+    private void requestLocationPermission(){
+        ActivityCompat.requestPermissions(getActivity(),
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_READ_CONTACTS_PERMISSION);
     }
 }
