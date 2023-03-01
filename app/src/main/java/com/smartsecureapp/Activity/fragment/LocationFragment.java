38cf17bb568dbb6e8c1f4a526089c0f38c426a38
Puneet <puneet.s@applify.co>
2023-03-01 20:16:31 +0530

    testing

--- app/src/main/java/com/smartsecureapp/Activity/fragment/LocationFragment.java
+++ app/src/main/java/com/smartsecureapp/Activity/fragment/LocationFragment.java
@@ -98,4 +98,5 @@ public class LocationFragment extends Fragment {
         ActivityCompat.requestPermissions(getActivity(),
                 new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_READ_CONTACTS_PERMISSION);
     }
+    //testing
 }
